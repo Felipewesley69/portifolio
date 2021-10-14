eb7b1e4ebf4c38ce8103b3bacd977393a3ae39ac
Felipe Lemos <felipewesley69@gmail.com>
2021-10-14 13:43:39 -0300

    Adicionado o form de contato

---
+++ script/main.js
@@ -0,0 +1,8 @@
+document.getElementById('name');
+document.getElementById('subject');
+document.getElementById('email');
+document.getElementById('phone');
+document.getElementById('message');
+document.getElementById('sendEmail').addEventListener('click', (e) => {
+
+})
